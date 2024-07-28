0c997448fa2b3f9a491f7b75108f68aa8ce857d9
radhaya9av <radhaya9av@gmail.com>
2024-07-28 09:59:16 +0530

    modalStyling

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useTheme } from '../context/themeContext';
 
 /*eslint-disable */
@@ -37,5 +38,88 @@ function Modal({setModal, giff, title, link, embed_url}) {
         </ModalStyled>
     )
 }
+
+const ModalStyled = styled.div`
+    .modal{
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        min-width: 30%;
+        transform: translate(-50%, -50%);
+        z-index: 13;
+        border-radius: 15px;
+        background: ${(props) => props.theme.colorBg1};
+        .modal-content{
+            padding: 2rem;
+            display: flex;
+            align-items: center;
+            gap: 2rem;
+            img{
+                border-radius: 5px;
+                border: 1px solid ${(props) => props.theme.colorBlue2};
+            }
+            .text-content{
+                h3{
+                    font-size: 1.6rem;
+                    text-align: center;
+                    padding-bottom: .5rem;
+                    margin-bottom: 2rem;
+                    font-weight: 800;
+                    border-bottom: 1px solid ${(props) => props.theme.colorBlue2};
+                    background: linear-gradient(to right,
+                        ${(props) => props.theme.colorSalmon},
+                        ${(props) => props.theme.colorBlue2},
+                        ${(props) => props.theme.colorGreen}
+                    );
+                    -webkit-background-clip: text;
+                    -webkit-text-fill-color: transparent;
+                    background-size: 400% 400%;
+                    animation: gradient 5s ease-in-out infinite;
+                    @keyframes gradient{
+                        0%{
+                            background-position: 0% 50%;
+                        }
+                        50%{
+                            background-position: 100% 50%;
+                        }
+                        100%{
+                            background-position: 0% 50%;
+                        }
+                    }
+                }
+
+                .share-item{
+                    margin-bottom: 1rem;
+                    font-size: 1.4rem;
+                    font-weight: 600;
+                    cursor: pointer;
+                    transition: all .3s ease-in-out;
+                    color: ${(props) => props.theme.colorBlue2};
+                    &:hover{
+                        transform: scale(1.1) translateX(25px);
+                    }
+                    a{
+                        font-size: inherit;
+                        font-family: inherit;
+                        color: inherit;
+                        text-decoration: none;
+                        display: grid;
+                        grid-template-columns: 40px 1fr;
+                        align-items: center;
+                    }
+                }
+            }
+        }
+    }
+    .overlay{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0,.6);
+        z-index: 12;
+    }
+`;
 
 export default Modal
