80c9db01d49de0c9156d841649ec3acf006103ed
radhaya9av <radhaya9av@gmail.com>
2024-07-28 08:46:07 +0530

    themeContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { GlobalStyle } from './styles/GlobalStyle';
+import { GlobalProvider } from './context/global';
+import { ThemeProvider } from './context/themeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <GlobalProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </GlobalProvider>
   </React.StrictMode>,
 )
